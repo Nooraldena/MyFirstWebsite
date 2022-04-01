1a8de1801299fea410dc79dfb01afed20857cfc8
noor <nooralden.12.n@gmail.com>
2022-04-02 01:22:03 +0300

    create Personal Website

---
+++ app.js
@@ -0,0 +1,42 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction} ;
+
+function scrollFunction(){
+  if(document.body.scrollTop > 20 ){  
+    mybutton.style.display="block";
+  }else{
+    mybutton.style.display="none";
+  }
+}
+
+function topFunction(){
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
